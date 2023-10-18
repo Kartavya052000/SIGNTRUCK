db6feed08efd2c5b471f80a7e9455cfaff604738
Kartavya Bhayana <kartavyabhayana1@gmail.com>
2023-10-18 02:08:08 -0700

    first commit

---
+++ src/Header/header.js
@@ -0,0 +1,14 @@
+// components/Header.js
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        HEADER
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
