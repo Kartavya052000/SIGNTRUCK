357526ab4d0afb10161fcaef55ce70714ea9d036
utkrishttiwari <utkrisht116@gmail.com>
2023-11-01 17:27:01 +0530

    login page fix css

--- src/login.js
+++ src/login.js
@@ -100,87 +100,91 @@ const handleLogIn = async (e) => {
     username: "",
   });
 };
-// const Login = () => {
+
+const [isLogSignup, setLogSignup] = useState(false);
+
+const logSignupClick = event => {
+  setLogSignup(current => !current);
+};
   return (
     <>
-      <section className='innerSec' style={{ backgroundImage: `url(${faqBanner})` }}>
-        <div className='custom-container'>
-          <h1>Login</h1>
-        </div>
-      </section>
-
+    <section className="innerSecBgHeader">
+      <div className="custom-container">
+        <h2>Login / SignUp</h2>
+      </div>
+    </section>
       <section className='login_sec'>
         <div className='login_inner'>
           <div className='custom-container'>
             <div className='loginForm'>
-              <form action="" class="form">
-                <input id="noaccount" name="radioaccount" type="radio" class="radio radio--invisible" checked />
-                <input id="account" name="radioaccount" type="radio" class="radio radio--invisible" />
-                <div class="form_background">
-                  <div class="form-group form-group--account">
-                    <h3 class="form-group_title">Sign up</h3>
+              <form action="" className="form">
+                <div className={isLogSignup ? 'form_background signup' : 'form_background'}>
+                  <div className="form-group form-group--account">
+                    <h3 className="form-group_title">Sign up</h3>
                     <div className='formGrp'>
                       <input
                         type="email"
                         name="email"
                         id="email"
                         value={email}
-                        placeholder="Enter your email"
+                        placeholder="Email Address"
                         onChange={handleOnChange}
                       />
                     </div>
                     <div className='formGrp'>
-                    <input
-            type="password"
-            name="password"
-            value={password}
-            placeholder="Enter your password"
-            onChange={handleOnChange}
-            id='password'
-          />
+                      <input
+                          type="password"
+                          name="password"
+                          value={password}
+                          placeholder="Password"
+                          onChange={handleOnChange}
+                          id='password'
+                        />
                     </div>
                     {/* <div className='formGrp'>
                       <input type='password' placeholder='Confirm Password' id='cpassword' />
                     </div> */}
                     <button type="button" className="butn butn_success button--form" onClick={handleSignUp}>Sign up</button>
                   </div>
-                  <div class="form-group form-group--noaccount">
-                    <h3 class="form-group_title">Log In</h3>
+                  <div className="form-group form-group--noaccount">
+                    <h3 className="form-group_title">Log In</h3>
                     <div className='formGrp'>
-                    <input
-                        type="email"
-                        name="loginemail"
-                        id="email2"
-                        value={loginemail}
-                        placeholder="Enter your email"
+                      <input
+                          type="email"
+                          name="loginemail"
+                          id="email2"
+                          value={loginemail}
+                          placeholder="Email Address"
+                          onChange={handleLoginOnChange}
+                      />
+                    </div>
+                    <div className='formGrp'>
+                      <input
+                        type="password"
+                        name="loginpassword"
+                        value={loginpassword}
+                        placeholder="Password"
                         onChange={handleLoginOnChange}
-                      />                    </div>
-                    <div className='formGrp'>
-                    <input
-            type="password"
-            name="loginpassword"
-            value={loginpassword}
-            placeholder="Enter your password"
-            onChange={handleLoginOnChange}
-            id='password2'
-          />                    </div>
+                        id='password2'
+                      />
+                    </div>
                     <div className='forgot'>
                       <span>Forgot Password?</span>
                     </div>
-                    <button type="button" class="butn butn_success button--form" onClick={handleLogIn}>Login</button>
+                    <button type="button" className="butn butn_success button--form" onClick={handleLogIn}>Login</button>
                   </div>
                 </div>
                 <fieldset className='fieldset noaccount'>
                   <h2>Have an account ?</h2>
                   <p>Banjo tote bag bicycle rights, High Life sartorial cray craft beer whatever street art fap.</p>
-                  <div class="form_content form_content--noaccount"></div>
-                  <label for="noaccount" class="button">login</label>
+                  <div className="form_content form_content--noaccount"></div>
+                  <label className='button' onClick={logSignupClick}>login</label>
                 </fieldset>
                 <fieldset className='fieldset account'>
                   <h2>Don't have an account ?</h2>
                   <p>Banjo tote bag bicycle rights, High Life sartorial cray craft beer whatever street art fap.</p>
-                  <div class="form_content form_content--noaccount"></div>
-                  <label for="account" class="button">Signup</label>
+                  <div className="form_content form_content--noaccount"></div>
+                  <label className='button' onClick={logSignupClick}>Signup</label>
                 </fieldset>
               </form>
             </div>
